e17da68d77bcede8145704789cee096b53f042c9
Cédric Merouani <cedric@merouani.net>
2022-03-30 09:09:03 +0200

    Updating aids export

--- src/Command/ExportAidsCommand.php
+++ src/Command/ExportAidsCommand.php
@@ -36,23 +36,26 @@ class ExportAidsCommand extends Command
                 fputcsv($outputBuffer, $aid, ',');
                 continue;
             }
-            $aid['fundingTypes'] = count($aid['fundingTypes']) > 0 ? "\"" . implode(',', $aid['fundingTypes']) . "\"" : null;
+            $fundingTypes = array_map(function($fundingType) {return str_contains($fundingType, '\"') ? $fundingType : "\"" . $fundingType . "\"";}, $aid['fundingTypes']);
+            $aid['fundingTypes'] = implode(',', $fundingTypes);
             $aid['funder'] = $aid['funder']['name'];
 
             foreach ($aid['environmentalTopics'] as $environmentalTopic) {
                 $environmentalTopicCategories = $environmentalTopic['environmentalTopicCategories'];
             }
 
-            $environmentalTopicCategoriesAndSectorsNames = array_map(function($topic) {return $topic['name'];}, $environmentalTopicCategories);
+            $environmentalTopicCategoriesAndSectorsNames = array_map(function($topic) {return "\"" . $topic['name'] . "\"";}, $environmentalTopicCategories);
+
             $environmentalTopicCategoriesNames = array_filter($environmentalTopicCategoriesAndSectorsNames, function ($categoryName) {
-                return !str_starts_with($categoryName, 'Secteur');
+                return !str_starts_with($categoryName, '"Secteur');
             });
-            $environmentalTopicSectorsName = array_filter($environmentalTopicCategoriesAndSectorsNames, function ($categoryName) {
-                return str_starts_with($categoryName, 'Secteur');
+            $environmentalTopicSectorsNames = array_filter($environmentalTopicCategoriesAndSectorsNames, function ($categoryName) {
+                return str_starts_with($categoryName, '"Secteur');
             });
-            $aid['environmentalTopicsCategories'] = count($environmentalTopicCategoriesNames) > 0 ? "\"" . implode(',', $environmentalTopicCategoriesNames) . "\"" : null;
-            $aid['activitySector'] = count($environmentalTopicSectorsName) > 0 ? "\"" . implode(',', $environmentalTopicSectorsName) . "\"" : null;
-            $aid['environmentalTopics'] = "\"" . implode(';', array_map(function($topic) {return $topic['name'] ;}, $aid['environmentalTopics'])) . "\"";
+
+            $aid['environmentalTopicsCategories'] = count($environmentalTopicCategoriesNames) > 0 ? implode(',', $environmentalTopicCategoriesNames) : null;
+            $aid['activitySector'] = count($environmentalTopicSectorsNames) > 0 ? implode(',',  $environmentalTopicSectorsNames )  : null;
+            $aid['environmentalTopics'] = implode(',', array_map(function($topic) {return "\"" . $topic['name'] . "\"" ;}, $aid['environmentalTopics'])) ;
             $aid['applicationEndDate'] = $aid['applicationEndDate'] !== null ? $aid['applicationEndDate']->format(\DateTime::ATOM) : null;
             $aid['applicationStartDate'] = $aid['applicationStartDate'] !== null ? $aid['applicationStartDate']->format(\DateTime::ATOM) : null;
             $aid['createdAt'] = $aid['createdAt'] !== null ? $aid['createdAt']->format(\DateTime::ATOM) : null;
