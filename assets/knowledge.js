bd9c8f6e47e07351658ef271862b71b1871658d8
Cédric Merouani <cedric@merouani.net>
2022-06-09 01:45:01 +0200

    Adding selectable css on tags

--- assets/knowledge.js
+++ assets/knowledge.js
@@ -2,7 +2,19 @@ import './styles/knowledge.scss';
 
 import Isotope from 'isotope-layout'
 
-document.addEventListener("DOMContentLoaded", function() {
+let showMoreButton = document.querySelector('#show-more > .button');
+showMoreButton.addEventListener('click', () => {
+    let showMoreItems = document.querySelectorAll('#show-more > .hidden');
+    for (let i = 0; i < 3; i++) {
+        showMoreItems[i].classList.toggle('hidden');
+    }
+
+    showMoreItems = document.querySelectorAll('#show-more > .hidden');
+    if (showMoreItems.length === 0) {
+        showMoreButton.classList.add('hidden');
+    }
+});
+
     var elem = document.querySelector('#tag-filter-content');
     var iso = new Isotope( elem, {
         // options
@@ -21,9 +33,11 @@ document.addEventListener("DOMContentLoaded", function() {
         let tags = document.getElementById('tag-filters');
         let activeTags = tags.getElementsByClassName('active');
         for (let tag of activeTags) {
-            tag.classList.remove('active')
+            tag.classList.remove('active');
+            tag.setAttribute('aria-pressed', 'false');
         }
         event.target.classList.add('active');
+        event.target.setAttribute('aria-pressed', 'true');
         var filterValue = event.target.getAttribute('data-filter');
         iso.arrange({ filter: filterValue });
 
@@ -46,36 +60,13 @@ document.addEventListener("DOMContentLoaded", function() {
         let tags = document.getElementById('funders-filters');
         let activeTags = tags.getElementsByClassName('active');
         for (let tag of activeTags) {
-            tag.classList.remove('active')
+            tag.classList.remove('active');
+            tag.setAttribute('aria-pressed', 'false');
         }
         event.target.classList.add('active');
+        event.target.setAttribute('aria-pressed', 'true');
         var filterValue = event.target.getAttribute('data-filter');
         iso2.arrange({ filter: filterValue });
-
-    }, false);
-
-    var elem3 = document.querySelector('#aids-filter-content');
-    var iso3 = new Isotope( elem3, {
-        // options
-        itemSelector: '.aids-content',
-        layoutMode: 'fitRows',
-        fitRows: {
-            gutter: 32
-        }
-    });
-
-    document.addEventListener('click', function (event) {
-
-        if (!event.target.matches('.sort-aids')) return;
-        event.preventDefault();
-        let tags = document.getElementById('aids-filters');
-        let activeTags = tags.getElementsByClassName('active');
-        for (let tag of activeTags) {
-            tag.classList.remove('active')
-        }
-        event.target.classList.add('active');
-        var filterValue = event.target.getAttribute('data-filter');
-        iso3.arrange({ filter: filterValue });
 
     }, false);
 
@@ -84,4 +75,3 @@ document.addEventListener("DOMContentLoaded", function() {
         let funderBaseTag = document.getElementById('ademe-filter');
         funderBaseTag.click();
     }, false);
-});
