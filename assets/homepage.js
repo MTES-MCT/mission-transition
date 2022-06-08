556c7471cd64e18855197c8b323042d35d3c8eea
Cédric Merouani <cedric@merouani.net>
2022-06-08 23:44:12 +0200

    Adding aids on homepage with show-more behavior

--- assets/homepage.js
+++ assets/homepage.js
@@ -3,9 +3,20 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import SearchEngineExtension from "./components/SearchEngineExtension";
 
-
 ReactDOM.render(
     <SearchEngineExtension/>,
     document.querySelector('#search-engine-extension')
 );
 
+let showMoreButton = document.querySelector('#show-more > .button');
+showMoreButton.addEventListener('click', () => {
+    let showMoreItems = document.querySelectorAll('#show-more > .hidden');
+    for (let i = 0; i < 3; i++) {
+        showMoreItems[i].classList.toggle('hidden');
+    }
+
+    showMoreItems = document.querySelectorAll('#show-more > .hidden');
+    if (showMoreItems.length === 0) {
+        showMoreButton.classList.add('hidden');
+    }
+});
