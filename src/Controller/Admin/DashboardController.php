d1d5dfec52bee9a5a9c0e7bd4af29f8d95c7f458
Cédric Merouani <cedric@merouani.net>
2023-02-13 14:42:17 +0100

    Changing crud default page to uncategorized aid

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -27,7 +27,7 @@ class DashboardController extends AbstractDashboardController
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
          $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-         return $this->redirect($adminUrlGenerator->setController(AideCrudController::class)->generateUrl());
+         return $this->redirect($adminUrlGenerator->setController(AideACategoriserCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
